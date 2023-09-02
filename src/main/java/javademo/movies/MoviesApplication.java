c4f9f221f5c4390f1e6ee430c7e271ff39a248ad
scicluna <88948700+scicluna@users.noreply.github.com>
2023-09-02 14:02:31 -0400

    initial commit

--- src/main/java/javademo/movies/MoviesApplication.java
+++ src/main/java/javademo/movies/MoviesApplication.java
@@ -2,7 +2,7 @@ package javademo.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,7 +10,6 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-
 
 
 }
